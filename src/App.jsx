d4b86ef67777f458149bc2e7e772d822eec0de15
Carlos Daniel Rigoni <icarlosrigoni@gmail.com>
2024-07-21 17:38:34 -0300

    refactor: Update Dashboard component layout and add FontAwesome tooltip
    Improve the layout of the Dashboard component by updating the grid structure and adding a tooltip to the FontAwesome icon.

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ library.add(faEnvelope, faUpload, faInfoCircle);
 function App() {
   return (
     <div className="flex flex-col items-center p-4">
-      <div className="flex items-center gap-12">
+      <div className="flex flex-col sm:flex-row items-center gap-12">
         <h1 className="text-2xl text-[#484446]">Account Overview</h1>
         <SupportCard />
       </div>
--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,4 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Tooltip } from "react-tooltip";
 
 export function Dashboard() {
   const uploads = 0;
@@ -12,20 +13,28 @@ export function Dashboard() {
           <FontAwesomeIcon icon="upload" className="mr-2" color="#3eb0eb" />
           <h2 className="font-semibold">Sales</h2>
         </div>
-        <FontAwesomeIcon icon="info-circle" className="mr-1" color="#bfbbbb" />
+        <FontAwesomeIcon
+          icon="info-circle"
+          className="mr-1"
+          color="#bfbbbb"
+          data-tooltip-id="infoTooltip"
+        />
+        <Tooltip id="infoTooltip" place="top" type="dark" effect="solid">
+          Additional sales information
+        </Tooltip>
       </div>
       <p className="my-4 text-sm text-[#4b4849] px-3">
         You had <strong>{uploads} uploads</strong> and{" "}
         <strong>{LinesAdded}</strong> lines added.
       </p>
 
-      <div className="grid grid-cols-2 border-t-[1px]">
-        <div className="border-r-2 p-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 border-t-[1px]">
+        <div className="sm:border-r-2 p-4">
           <p className="text-[#22ab55] font-bold text-2xl">{uploads}%</p>
           <p className="text-[#9e9c9c] text-sm font-semibold">UPLOAD SUCCESS</p>
         </div>
 
-        <div className="p-4">
+        <div className="p-4 border-t-[1px] sm:border-0">
           <p className="text-[#22ab55] font-bold text-2xl">{linesSaved}%</p>
           <p className="text-[#9e9c9c] text-sm font-semibold">LINES SAVED</p>
         </div>
