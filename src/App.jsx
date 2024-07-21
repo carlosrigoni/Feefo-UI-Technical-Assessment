d22b930e27c378d6118d02d8b35bcd22e932abd4
Carlos Daniel Rigoni <icarlosrigoni@gmail.com>
2024-07-21 16:58:14 -0300

    refactor: Improve App.jsx layout and add Dashboard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import { Dashboard } from "./components/Dashboard/Dashboard";
 import { SupportCard } from "./components/SupportCard/SupportCard";
 
 function App() {
   return (
-    <div className="flex justify-center p-4">
+    <div className="flex flex-col items-center p-4">
       <div className="flex items-center gap-12">
-        <h1 className="text-2xl text-[#484446]">Account overview</h1>
+        <h1 className="text-2xl text-[#484446]">Account Overview</h1>
         <SupportCard />
       </div>
+
+      <Dashboard />
     </div>
   );
 }
