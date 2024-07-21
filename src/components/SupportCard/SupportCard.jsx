683e501291c8ecaaac2ffd677ff085e891c76590
Carlos Daniel Rigoni <icarlosrigoni@gmail.com>
2024-07-21 18:03:14 -0300

    refactor: Update SupportCard component layout and styles
    Improve the layout and styles of the SupportCard component by adjusting the font color and size, as well as the background color of the support icon.

--- src/components/SupportCard/SupportCard.jsx
+++ src/components/SupportCard/SupportCard.jsx
@@ -4,15 +4,15 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export function SupportCard() {
   return (
     <div>
-      <h2 className="text-gray-500 text-sm font-semibold tracking-wider">
+      <h2 className="text-gray-400 text-sm font-semibold">
         YOUR FEEFO SUPPORT CONTACT
       </h2>
       <div className="flex items-center mt-2">
-        <div className="bg-yellow-400 w-12 h-12 rounded-md flex justify-center items-center mr-2">
+        <div className="bg-yellow-400 w-11 h-11 rounded-md flex justify-center items-center mr-2">
           <p className="font-semibold">S</p>
         </div>
         <div>
-          <p className="font-semibold text-sm text-gray-700">Support</p>
+          <p className="font-semibold text-sm text-gray-600">Support</p>
           <div className="flex justify-between text-xs text-gray-600">
             <div className="flex items-center">
               <FontAwesomeIcon
