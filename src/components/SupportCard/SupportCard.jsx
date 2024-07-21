8d722f45df9f6306f859012225bceadcb1fc6e4b
Carlos Daniel Rigoni <icarlosrigoni@gmail.com>
2024-07-21 16:25:11 -0300

    feat: Add SupportCard component
    The SupportCard component is added to display the contact information for Feefo support.

---
+++ src/components/SupportCard/SupportCard.jsx
@@ -0,0 +1,22 @@
+export function SupportCard() {
+  return (
+    <div>
+      <h2 className="text-[#9e9c9c] text-sm font-semibold tracking-wider">
+        YOUR FEEFO SUPPORT CONTACT
+      </h2>
+      <div className="flex items-center mt-2 ">
+        <div className="bg-[#f9cf03] w-12 h-12 rounded-md flex justify-center items-center mr-2">
+          <p className="font-semibold">S</p>
+        </div>
+
+        <div className="">
+          <p className="font-semibold text-sm text-[#4b4849]">Support</p>
+          <div className="flex  justify-between text-xs text-[#606060]">
+            <p className="mr-4">support@feefo.com</p>
+            <p>020 3362 4208</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
